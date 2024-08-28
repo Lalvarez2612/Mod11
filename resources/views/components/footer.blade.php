69228b983010bc6a837890752f132c0a86a8abe3
lapq14 <luispetitqa@gmail.com>
2024-08-28 13:13:27 -0400

    arreglos footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,17 +1,17 @@
-<footer class="footer mt-5  p-3 text-white w-full">
+<footer class="footer mt-5  p-3 w-full">
     
-    <div class="d-flex justify-content-between ">
-        <div>
-            <h4 class="font-bold text-lg mr-96">Contacto</h4>
+    <div class="links d-flex justify-content-between ">
+        <div class="titu">
+            <h4 class="mb-2">Contacto</h4>
             <p>Teléfono: +58 (424) 203-3409</p>
             <p>Email: thetoprest@gmail.com</p>
             <p>Dirección: Los flores de Catia, Caracas</p>
         </div>
         <div>
-            <h4 class="font-bold text-lg ">Síguenos en redes</h4>
-            <a href="https://www.facebook.com/photo/?fbid=927148446123725&set=a.573976618107578" class="text-white rounded-xl px-2 py-1 mr-2 hover:bg-orange-900">Facebook</a>
-            <a href="https://www.instagram.com/reel/C88cr6NMCkq/?utm_source=ig_web_copy_link" class="text-white rounded-xl px-2 py-1 mr-2 hover:bg-orange-900">Instagram</a>
-            <a href="https://x.com/MariaCorinaYA/status/1826607889244668019" class="text-white rounded-xl px-2 py-1 hover:bg-orange-900">X (Twitter)</a>
+            <h4 class="titu mb-2">Síguenos en redes</h4>
+            <a href="https://www.facebook.com/photo/?fbid=927148446123725&set=a.573976618107578" class=" rounded-xl px-2 py-1 mr-2 ">Facebook</a>
+            <a href="https://www.instagram.com/reel/C88cr6NMCkq/?utm_source=ig_web_copy_link" class="rounded-xl px-2 py-1 mr-2 ">Instagram</a>
+            <a href="https://x.com/MariaCorinaYA/status/1826607889244668019" class="rounded-xl px-2 py-1 ">X (Twitter)</a>
         </div>
         
     </div>
